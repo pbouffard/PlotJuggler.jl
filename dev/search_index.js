f9895bd93665211bd6e89877c687d726ca926fb8
Documenter.jl <documenter@juliadocs.github.io>
2022-08-13 23:06:09 +0000

    build based on bb64826

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PlotJuggler","category":"page"},{"location":"#PlotJuggler","page":"Home","title":"PlotJuggler","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PlotJuggler.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PlotJuggler]","category":"page"}]
+}

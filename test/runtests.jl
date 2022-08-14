d924aaaaef798ad7de1280f82a74bd3e33a9055a
Patrick Bouffard <airpmb@fastmail.com>
2022-08-14 09:05:39 -0700

    default colors

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,5 +7,5 @@ using Test
     freq = 2.0
     a = cos.(2*pi/freq*t)
     b = sin.(2*pi/freq*t)
-    PlotJuggler.pjplot(t, [a, b])
+    PlotJuggler.pjplot(t, [a, b, 2*a, 2*b, 3*a, 3*b, 4*a, 4*b, 5*a, 5*b])
 end

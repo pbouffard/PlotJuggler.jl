ec8763a1195b8f6f0dd820825888b7a4dd473c69
Patrick Bouffard <airpmb@fastmail.com>
2022-08-14 10:31:02 -0700

    docs, more methods

--- src/PlotJuggler.jl
+++ src/PlotJuggler.jl
@@ -1,24 +1,74 @@
 module PlotJuggler
 
+export pjplot
+
 using DelimitedFiles
 
 include("xml_templates.jl")
 
 # Write your package code here.
 
-pjpath = "/Users/patrick/dev/PlotJuggler/install/bin/plotjuggler" # TODO: make customizable
+pjpath = try
+        strip(read(`which plotjuggler`, String))
+    catch
+        defaultpath = "/Users/patrick/dev/PlotJuggler/install/bin/plotjuggler" # TODO: make customizable
+        println("Warning: using default path of $(defaultpath)")
+        defaultpath
+    end
 
-defaultcolors = ["#1f77b4", "#d62728", "#1ac938", "#ff7f0e", "#f14cc1", "#9467bd", "#17becf", "#bcbd22", "#1f77b4"]
+defaultcolors = ["#1f77b4", "#d62728", "#1ac938", "#ff7f0e", "#f14cc1", "#9467bd", "#17becf", "#bcbd22", "#1f77b4", "#000000"]
 
-function pjplot(t, curves)
+"""
+    pjplot(t, curves)
+
+Start a PlotJuggler session with `t` as the time variable and `curves` the data to plot against time. Each curve will be given a distinct color (up to 10, then the colors repeat) solid line.
+
+# Examples
+For convenience the `curves` argument can take several forms. The most common would be to pass a [`NamedTuple`](@ref) which will allow the names of the variables in the calling scope to be picked up as the curve names automatically:
+
+```
+t = 0:0.1:5
+freq = 2.0
+a = cos.(2*pi/freq*t)
+b = sin.(2*pi/freq*t)
+
+pjplot(t, (; a, b))
+```
+
+If a simple `Vector` (or compatible type) is passed in for the second argument then it will be plotted against time and named `data1` in the plot:
+```
+pjplot(t, 2*t)
+```
+
+If a Vector of Vectors is passed in for the second argument then each one will be named as `data1`, `data2`, etc.:
+```
+pjplot(t, [t, 2*t, 4*t])
+```
+"""
+function pjplot(t, curves::AbstractVector{T}) where {T <: AbstractVector}
+    @show "Vector of Vectors"
+    curvenames = "data" .* string.(1:length(curves))
+    data = (; collect(zip(Symbol.(curvenames), curves))...)
+    #return data
+    pjplot(t, data)
+end
+
+function pjplot(t, data::AbstractVector{T}) where {T <: Number}
+    @show "Vector"
+    pjplot(t, [data])
+end
+
+function pjplot(t, curves::T) where {T <: NamedTuple}
+    @show "NamedTuple vector"
     # Generate CSV file
     path = tempname(cleanup=false) * ".csv"
     @show path
-    curvenames = "data" .* string.(1:length(curves))
+    curvenames = string.(keys(curves))
+    @show curvenames
     open(path, "w") do io
-        write(io, join(["t"; curvenames], ","))
+        write(io, join(["t", curvenames...], ","))
         write(io, "\n")
-        writedlm(io, [t curves...], ',')
+        writedlm(io, [t values(curves)...], ',')
     end
 
     # Generate layout file
@@ -29,7 +79,7 @@ function pjplot(t, curves)
     @show pjcmd
     # start PJ (TODO - surely there's a better way to spawn PJ and return immediately..?)
     pjbashcmd = `/bin/bash -c "$(pjcmd)"\&`
-    run(Cmd(pjcmd; detach=true))
+    run(Cmd(pjcmd; detach=false))
 end
 
 function writelayoutfile(datapath, curvenames)
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,27 @@ using Test
 
 @testset "PlotJuggler.jl" begin
     # Write your tests here.
-    t = 0:0.1:10
+    t = 0:0.01:5
     freq = 2.0
     a = cos.(2*pi/freq*t)
     b = sin.(2*pi/freq*t)
-    PlotJuggler.pjplot(t, [a, b, 2*a, 2*b, 3*a, 3*b, 4*a, 4*b, 5*a, 5*b])
+    
+    println("Vector (ints)")
+    pjplot([0, 1, 2], [1, 2, 4])
+
+    println("Vector (anonymous)")
+    pjplot(t, a)
+
+    println("Vector (named)")
+    pjplot(t, (; a))
+
+    println("NamedTuple")
+    curves2 = (;a, b, ab=a.*b, exp_t=exp.(-t))  # <: NamedTuple
+    pjplot(t, curves2)
+
+    println("Vector of Vectors")
+    curves = [a, b, 2*a, 2*b, 3*a, 3*b, 4*a, 4*b, 5*a, 5*b] # <: Vector
+    result = pjplot(t, curves)
+    @show result
+
 end
